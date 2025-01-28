8bb74918e3c1ec4a4ff54b69293520d1fadc1d04
harivardhan <harivardhan.yekula@quantana.in>
2025-01-28 19:33:28 +0530

    adsfasDF

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,39 @@
-module.exports = () => ({});
+module.exports = () => ({
+    'preview-button': {
+        config: {
+          enabled: true,
+          contentTypes: [
+            {
+              uid: 'api::page.page',
+              draft: {
+                url: 'http://localhost:3000/api/preview',
+                query: {
+                  type: 'page',
+                  slug: '{slug}',
+                },
+                openTarget: 'StrapiPreviewPage',
+              },
+              published: {
+                url: 'http://localhost:3000/{slug}',
+                openTarget: 'StrapiPage',
+              },
+            },
+            {
+              uid: 'api::post.post',
+              draft: {
+                url: 'http://localhost:3000/api/preview',
+                query: {
+                  type: 'post',
+                  slug: '{slug}',
+                },
+                openTarget: 'StrapiPreviewPost',
+              },
+              published: {
+                url: 'http://localhost:3000/blog/{slug}',
+                openTarget: 'StrapiPost',
+              },
+            },
+          ],
+        },
+      },
+});
